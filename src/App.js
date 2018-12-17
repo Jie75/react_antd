cfc42825bfad63bdebb95bddda0c23fd01acf0e8
李世杰 <lisj@digimagus.com>
2018-12-17 18:08:58 +0800

    main 布局

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'antd';
 
+
 class App extends Component {
   render() {
     return (
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+
+    render() {
+        return (
+            <span>this is header</span>
+        );
+    }
+}
